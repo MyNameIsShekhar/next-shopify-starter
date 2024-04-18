02f8442f0a85f09d4f242788d553778363317672
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-04-18 23:14:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,13 @@
 // pages/index.js
 
 import SearchBar from '@/components/SearchBar'; // Adjust the import path as needed
-import StoreHeading from '@/components/StoreHeading';
-import ProductListings from '@/components/ProductListings';
-import { getAllProductsInCollection } from '@/lib/shopify';
 
-function IndexPage({ products }) {
+function IndexPage() {
   return (
     <div className="mx-auto max-w-6xl">
       <SearchBar /> {/* Add the SearchBar component here */}
-      <StoreHeading />
-      <ProductListings products={products} />      
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const products = await getAllProductsInCollection();
-
-  return {
-    props: {
-      products
-    },
-  };
 }
 
 export default IndexPage;
